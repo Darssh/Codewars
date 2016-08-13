ba3fed90eeb07edebc0d862f893d4203e296b15b
Darssh <darshitsuthar91@gmail.com>
2016-08-12 17:12:59 -0700

    Simple Encryption

--- Simple Encryption #1 - Alternating Split.py
+++ Simple Encryption #1 - Alternating Split.py
@@ -18,19 +18,19 @@ Have fun coding it and please don't forget to vote and rank this kata! :-)'''
 
 
 def decrypt(encrypted_text, n):
-	for k in range(n):
-		t = encrypted_text + encrypted_text
-    	string = [None] * (len(encrypted_text))
-    	for i in range(0,len(encrypted_text)/2,1):
-    		string[i*2 + 1] = str(t[i])
-    	j = 0
-    	for i in range(len(encrypted_text)/2,len(encrypted_text),1):
-    		string[j] = str(t[i])
-    		j += 2
-    	encrypted_text = ''.join(string)
+    for k in range(n):
+        t = encrypted_text + encrypted_text
+        string = [None] * len(encrypted_text)
+        for i in range(0,int(len(encrypted_text)/2),1):
+            string[i*2 + 1] = str(t[i])
+        j = 0
+        for i in range(int(len(encrypted_text)/2),int(len(encrypted_text)),1):
+            string[j] = str(t[i])
+            j += 2
+        encrypted_text = ''.join(string)
+    return encrypted_text	
 
-	return encrypted_text	
-
+    
 def encrypt(text, n):
 	for i in range(n):
 		if(len(text)%2 == 0):
@@ -46,4 +46,21 @@ def encrypt(text, n):
 	return text
 
 
-print(encrypt("This kata is very interesting!", 1))
+'''def decrypt(text, n):
+    if text in ("", None):
+        return text
+    
+    ndx = len(text) // 2
+
+    for i in range(n):
+        a = text[:ndx]
+        b = text[ndx:]
+        text = "".join(b[i:i+1] + a[i:i+1] for i in range(ndx + 1))
+    return text
+
+
+
+def encrypt(text, n):
+    for i in range(n):
+        text = text[1::2] + text[::2]
+    return text'''
